092c5328bfff9714b2f8d60bea97a3c319dd3093
ankitshiv <ankitshiv91@gmail.com>
2020-02-22 15:25:00 -0800

    initial commit

---
+++ src/config/config.ts
@@ -0,0 +1,19 @@
+export const config = {
+  "dev": {
+    "username": "udashivharedev",
+    "password": "udashivharedev",
+    "database": "udashivharedev",
+    "host": "udashivharedev.cpuxt671cxap.us-east-2.rds.amazonaws.com",
+    "dialect": "postgres",
+    "aws_region": "us-east-2",
+    "aws_profile": "default",
+    "aws_media_bucket": "udashivharedev"
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "udagram_prod",
+    "host": "",
+    "dialect": "postgres"
+  }
+}
